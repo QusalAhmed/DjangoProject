a2ed1dd4001a75d3106617384a4570fac2873906
Qusal <qusal15@student.sust.edu>
2024-12-12 00:23:59 +0600

    Initial

--- molasses/models.py
+++ molasses/models.py
@@ -15,6 +15,7 @@ class Product(models.Model):
     product_url = models.SlugField(default="", editable=False, unique=True, max_length=2000)
     seo_title = models.TextField()
     category = models.CharField(max_length=100)
+    rank = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
