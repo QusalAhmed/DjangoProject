675d0b6ea859570baba84fce42de6e0c9d41c59f
Qusal <qusal15@student.sust.edu>
2024-12-12 02:07:53 +0600

    Initial

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles/static'
 STATIC_URL = 'static/'
 # Define the path to the global static directory
 STATICFILES_DIRS = [
