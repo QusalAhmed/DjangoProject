4fdd3a8fb4c7b8f1937d2c4752e381bf0402fc94
Qusal <qusal15@student.sust.edu>
2025-01-05 17:53:30 +0600

    Initial

--- static/general.js
+++ static/general.js
@@ -12,6 +12,39 @@ if (!currentUrl.includes('localhost:8000')) {
     });
 }
 
+// Toast
+// Function to Show Toast
+function showToast(message) {
+    // Get the toast container
+    const toastContainer = document.getElementById('toast-box');
+
+    // Create a new toast element
+    const toast = document.createElement('div');
+    toast.className = 'custom-toast';
+    toast.textContent = message;
+
+    // Append the toast to the container
+    toastContainer.appendChild(toast);
+
+    // Show the toast
+    setTimeout(() => {
+        toast.classList.add('show');
+    }, 100);
+
+    // Remove the toast after 3 seconds
+    setTimeout(() => {
+        toast.classList.remove('show');
+        // Wait for the animation to complete before removing from DOM
+        setTimeout(() => {
+            toast.remove();
+        }, 300);
+    }, 1000);
+}
+
+$(document).ready(function () {
+    showToast('You are logged in as Guest');
+});
+
 // Bootstrap tooltips activation
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
