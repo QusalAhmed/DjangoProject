b7f496c109bf4a9c94272affb1d8ae76bfab5cb2
Qusal <qusal15@student.sust.edu>
2025-01-10 02:03:53 +0600

    Initial

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -30,8 +30,6 @@ ALLOWED_HOSTS = [
     'jazakallah.store',
     'www.jazakallah.store',
     '198.23.217.35',
-    '127.0.0.1',  # Optional for local development
-    'localhost',  # Optional for local development
 ]
 # ALLOWED_HOSTS = ['*']
 
@@ -199,5 +197,35 @@ JAZZMIN_SETTINGS = {
     "site_logo": "/logo.png",
     "copyright": "Jazakallah Store",
     "show_ui_builder": True,
-    "theme": "cosmo",
+}
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": False,
+    "accent": "accent-primary",
+    "navbar": "navbar-white navbar-light",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-light-lightblue",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "flatly",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-outline-primary",
+        "secondary": "btn-outline-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
 }
